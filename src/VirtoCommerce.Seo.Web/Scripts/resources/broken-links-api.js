d1e9c4494d4de9d60d751ce3578fecad0765ecef
Basil Kotov <basilkot@gmail.com>
2025-05-20 20:15:25 +0100

    remove unnecessary files

--- src/VirtoCommerce.Seo.Web/Scripts/resources/broken-links-api.js
+++
@@ -1,4 +0,0 @@
-angular.module('VirtoCommerce.BrokenLinks')
-    .factory('VirtoCommerce.BrokenLinks.webApi', ['$resource', function ($resource) {
-        return $resource('api/broken-links');
-    }]);
