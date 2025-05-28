4c75f5fd9f8acc92805af94d96258998d3aa57c4
Artem Dudarev <artem@virtoworks.com>
2025-05-28 11:59:00 +0200

    Update platform

--- src/VirtoCommerce.Seo.Web/Module.cs
+++ src/VirtoCommerce.Seo.Web/Module.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
+using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.Seo.Core;
 using VirtoCommerce.Seo.Core.Extensions;
 using VirtoCommerce.Seo.Core.Services;
 using VirtoCommerce.Seo.Data.Services;
@@ -30,6 +33,15 @@ public class Module : IModule, IHasConfiguration
 
     public void PostInitialize(IApplicationBuilder appBuilder)
     {
+        var serviceProvider = appBuilder.ApplicationServices;
+
+        // Register settings
+        var settingsRegistrar = serviceProvider.GetRequiredService<ISettingsRegistrar>();
+        settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
+
+        // Register permissions
+        var permissionsRegistrar = serviceProvider.GetRequiredService<IPermissionsRegistrar>();
+        permissionsRegistrar.RegisterPermissions(ModuleInfo.Id, "Seo", ModuleConstants.Security.Permissions.AllPermissions);
     }
 
     public void Uninstall()
