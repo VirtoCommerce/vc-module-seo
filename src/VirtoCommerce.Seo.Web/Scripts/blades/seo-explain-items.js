9aeb0ce14ad5819d65c8bb838a261948eda57098
Sergey Sorokin <Sergey.Sorokin@virtoworks.com>
2025-10-09 12:22:43 +0400

    Edited blade seo-explain-items

--- src/VirtoCommerce.Seo.Web/Scripts/blades/seo-explain-items.js
+++ src/VirtoCommerce.Seo.Web/Scripts/blades/seo-explain-items.js
@@ -2,59 +2,15 @@ angular.module('virtoCommerce.seo')
     .controller('virtoCommerce.seo.seoExplainItemsController', [
         '$scope',
         function ($scope) {
-
             var blade = $scope.blade;
             blade.title = 'seo.blades.seo-explain-items.title';
             blade.headIcon = 'fa fa-table';
-            blade.isLoading = true;
+            blade.isLoading = false;
 
             $scope.gridOptions = {
+                data: 'blade.items',
                 enableColumnMenus: false,
-                rowHeight: 32,
-                columnDefs: [
-                    {
-                        name: 'seoInfo.pageTitle',
-                        displayName: 'seo.blades.seo-explain-items.labels.page-title',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'seoInfo.semanticUrl',
-                        displayName: 'seo.blades.seo-explain-items.labels.semantic-url',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'seoInfo.objectType',
-                        displayName: 'seo.blades.seo-explain-items.labels.object-type',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'seoInfo.languageCode',
-                        displayName: 'seo.blades.seo-explain-items.labels.language',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'seoInfo.storeId',
-                        displayName: 'seo.blades.seo-explain-items.labels.store',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'score',
-                        displayName: 'seo.blades.seo-explain-items.labels.score',
-                        type: 'number',
-                        headerTooltip: true
-                    },
-                    {
-                        name: 'objectTypePriority',
-                        displayName: 'seo.blades.seo-explain-items.labels.priority',
-                        type: 'number',
-                        headerTooltip: true
-                    }
-                ],
-                data: blade.items
+                rowHeight: 40
             };
-
-            $scope.gridOptions.data = blade.items;
-
-            blade.isLoading = false;
         }
     ]);
