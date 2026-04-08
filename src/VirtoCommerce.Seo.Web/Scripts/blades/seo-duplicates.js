9758be09b8e0e24ab139184d8f5a148374130e2c
Basil Kotov <basilkot@gmail.com>
2026-04-08 11:36:32 +0100

    format js file

--- src/VirtoCommerce.Seo.Web/Scripts/blades/seo-duplicates.js
+++ src/VirtoCommerce.Seo.Web/Scripts/blades/seo-duplicates.js
@@ -1,115 +1,119 @@
 angular.module('virtoCommerce.seo')
-    .controller('virtoCommerce.seo.seoDuplicatesController', ['$rootScope', '$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.seo.seoApi', function ($rootScope, $scope, bladeNavigationService, seoApi) {
-        const blade = $scope.blade;
-        let anyValidSeoFound;
+    .controller('virtoCommerce.seo.seoDuplicatesController',
+        ['$rootScope', '$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.seo.seoApi',
+            function ($rootScope, $scope, bladeNavigationService, seoApi) {
+                const blade = $scope.blade;
+                let anyValidSeoFound;
 
-        function initializeBlade() {
-            blade.origEntity = blade.duplicates;
-            blade.currentEntities = angular.copy(blade.origEntity);
-            blade.isLoading = false;
-        };
-
-        function validate(data) {
-            // check all restrictions
-            const isUrlValid = data.semanticUrl && $scope.semanticUrlValidator(data.semanticUrl);
-            const retVal = isUrlValid && $scope.duplicateValidator(data.semanticUrl, data);
-            if (isUrlValid && !retVal) {
-                blade.conflicts[data.id] = true;
-            }
-            anyValidSeoFound = anyValidSeoFound || retVal;
-            return retVal;
-        }
-
-        $scope.semanticUrlValidator = function (value) {
-            const pattern = /[$+;=%{}[\]|\\/@ ~#!^*&?:'<>,]/;
-            return !pattern.test(value);
-        };
-
-        $scope.duplicateValidator = function (value, seoInfo) {
-            return _.all(blade.currentEntities, function (x) {
-                return x === seoInfo ||
-                    x.storeId !== seoInfo.storeId ||
-                    x.semanticUrl !== value;
-            });
-        };
-
-        $scope.saveChanges = function () {
-            blade.isLoading = true;
-
-            // generate data to save
-            const dataToSave = [];
-            _.each(blade.currentEntities, function (x) {
-                if (validate(x) &&
-                    !angular.equals(x, _.findWhere(blade.origEntity, { id: x.id }))) {
-                    x = angular.copy(x);
-                    if (!x.storeId) {
-                        x.id = undefined;
-                        x.storeId = blade.defaultContainerId;
-                    }
-                    dataToSave.push(x);
+                function initializeBlade() {
+                    blade.origEntity = blade.duplicates;
+                    blade.currentEntities = angular.copy(blade.origEntity);
+                    blade.isLoading = false;
                 }
-            });
 
-            seoApi.batchUpdate(dataToSave, function () {
-                angular.copy(blade.currentEntities, blade.origEntity);
-
-                seoApi.query({ objectId: blade.seoContainerObject.id, objectType: blade.objectType }, function (results) {
-                    if (_.any(results)) { // conflicts still found
-                        blade.duplicates = results;
-                        initializeBlade();
-                    } else {
-                        if (!blade.noClose) {
-                            $scope.bladeClose();
-                        }
-                        $rootScope.$broadcast("refresh-entity-by-id", blade.seoContainerObject.id);
+                function validate(data) {
+                    // check all restrictions
+                    const isUrlValid = data.semanticUrl && $scope.semanticUrlValidator(data.semanticUrl);
+                    const retVal = isUrlValid && $scope.duplicateValidator(data.semanticUrl, data);
+                    if (isUrlValid && !retVal) {
+                        blade.conflicts[data.id] = true;
                     }
-                });
-            }, function (error) { bladeNavigationService.setError('Error: ' + error.status, blade); });
-        };
+                    anyValidSeoFound = anyValidSeoFound || retVal;
+                    return retVal;
+                }
 
-        function saveChanges_noClose() {
-            blade.noClose = true;
-            $scope.saveChanges();
-        }
+                $scope.semanticUrlValidator = function (value) {
+                    const pattern = /[$+;=%{}[\]|\\/@ ~#!^*&?:'<>,]/;
+                    return !pattern.test(value);
+                };
 
-        function isDirty() {
-            return !angular.equals(blade.currentEntities, blade.origEntity) && blade.hasUpdatePermission();
-        }
+                $scope.duplicateValidator = function (value, seoInfo) {
+                    return _.all(blade.currentEntities, function (x) {
+                        return x === seoInfo ||
+                            x.storeId !== seoInfo.storeId ||
+                            x.semanticUrl !== value;
+                    });
+                };
 
-        function canSave() {
-            return isDirty() && anyValidSeoFound;
-        }
+                $scope.saveChanges = function () {
+                    blade.isLoading = true;
 
-        blade.onClose = function (closeCallback) {
-            bladeNavigationService.showConfirmationIfNeeded(isDirty(), canSave(), blade, saveChanges_noClose, closeCallback, "seo.dialogs.seo-save.title", "seo.dialogs.seo-save.message");
-        };
+                    // generate data to save
+                    const dataToSave = [];
+                    _.each(blade.currentEntities, function (x) {
+                        if (validate(x) &&
+                            !angular.equals(x, _.findWhere(blade.origEntity, { id: x.id }))) {
+                            x = angular.copy(x);
+                            if (!x.storeId) {
+                                x.id = undefined;
+                                x.storeId = blade.defaultContainerId;
+                            }
+                            dataToSave.push(x);
+                        }
+                    });
 
-        blade.toolbarCommands = [
-            {
-                name: "platform.commands.save", icon: 'fas fa-save',
-                executeMethod: $scope.saveChanges,
-                canExecuteMethod: canSave,
-                permission: blade.updatePermission
-            },
-            {
-                name: "platform.commands.reset", icon: 'fa fa-undo',
-                executeMethod: function () {
-                    angular.copy(blade.origEntity, blade.currentEntities);
-                },
-                canExecuteMethod: isDirty,
-                permission: blade.updatePermission
-            }
-        ];
+                    seoApi.batchUpdate(dataToSave, function () {
+                        angular.copy(blade.currentEntities, blade.origEntity);
 
-        blade.title = blade.parentBlade.title;
-        blade.subtitle = 'seo.blades.seo-duplicates.subtitle';
+                        seoApi.query({ objectId: blade.seoContainerObject.id, objectType: blade.objectType }, function (results) {
+                            if (_.any(results)) { // conflicts still found
+                                blade.duplicates = results;
+                                initializeBlade();
+                            } else {
+                                if (!blade.noClose) {
+                                    $scope.bladeClose();
+                                }
+                                $rootScope.$broadcast("refresh-entity-by-id", blade.seoContainerObject.id);
+                            }
+                        });
+                    }, function (error) { bladeNavigationService.setError('Error: ' + error.status, blade); });
+                };
 
-        initializeBlade();
+                function saveChanges_noClose() {
+                    blade.noClose = true;
+                    $scope.saveChanges();
+                }
 
-        $scope.$watch('blade.currentEntities', function () {
-            blade.conflicts = {};
-            anyValidSeoFound = false;
-            // must check ALL SEOs
-            _.each(blade.currentEntities, validate);
-        }, true);
-    }]);
+                function isDirty() {
+                    return !angular.equals(blade.currentEntities, blade.origEntity) && blade.hasUpdatePermission();
+                }
+
+                function canSave() {
+                    return isDirty() && anyValidSeoFound;
+                }
+
+                blade.onClose = function (closeCallback) {
+                    bladeNavigationService.showConfirmationIfNeeded(
+                        isDirty(), canSave(), blade, saveChanges_noClose, closeCallback,
+                        "seo.dialogs.seo-save.title", "seo.dialogs.seo-save.message");
+                };
+
+                blade.toolbarCommands = [
+                    {
+                        name: "platform.commands.save", icon: 'fas fa-save',
+                        executeMethod: $scope.saveChanges,
+                        canExecuteMethod: canSave,
+                        permission: blade.updatePermission
+                    },
+                    {
+                        name: "platform.commands.reset", icon: 'fa fa-undo',
+                        executeMethod: function () {
+                            angular.copy(blade.origEntity, blade.currentEntities);
+                        },
+                        canExecuteMethod: isDirty,
+                        permission: blade.updatePermission
+                    }
+                ];
+
+                blade.title = blade.parentBlade.title;
+                blade.subtitle = 'seo.blades.seo-duplicates.subtitle';
+
+                initializeBlade();
+
+                $scope.$watch('blade.currentEntities', function () {
+                    blade.conflicts = {};
+                    anyValidSeoFound = false;
+                    // must check ALL SEOs
+                    _.each(blade.currentEntities, validate);
+                }, true);
+            }]);
